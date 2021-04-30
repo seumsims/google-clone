284ac5992387860c73957c70705e9a9d6822333b
Tester <seumworldinbangladesh@gmail.com>
2021-04-30 11:17:09 +0600

    error detected switch takes twich

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Home from './pages/Home';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
-} from 'react-router-dom';
+} from "react-router-dom";
 
 function App() {
   return (
