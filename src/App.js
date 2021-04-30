db79a71928c5cc650a12ad2a15ad46610c88ca02
Tester <seumworldinbangladesh@gmail.com>
2021-04-30 09:22:24 +0600

    takes home js on components and install es7 snippets ext on vs code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>
      SEUM SALAMOON ALAIKOOM SALAMOON SALAMOON
+     </h1>
+     <Home/>
     </div>
   );
 }
