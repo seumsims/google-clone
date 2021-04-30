01b58eed40d0cfc9d1a41e69c76eab0dcf8885ba
Tester <seumworldinbangladesh@gmail.com>
2021-04-30 10:55:17 +0600

    Set Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div className='home'>
-            <h1>SALAM from Home</h1>
+            <h1>SALAM from Home Js</h1>
             <div className='home__header'>
                 <div className='home__headerLeft'>
-                    <Link to='about'>About</Link>
+                    <Link to='/about'>About</Link>
 
                 </div>
 
