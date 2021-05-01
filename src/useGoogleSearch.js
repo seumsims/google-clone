f75d5e3c2ccf3dc173d53bbc0303d0cf86f3dd2a
Tester <seumworldinbangladesh@gmail.com>
2021-05-02 05:09:38 +0600

    takes use google search and setting that

---
+++ src/useGoogleSearch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+import API_KEY from './keys';
+
+const CONTEXT_KEY = "9c02f919021fa6113";
+
+const useGoogleSearch = () => {
+    const [data, setData] = useState[null];
+
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        }
+    }, [input])
+    
+}
+
+export default useGoogleSearch
