8719d504005160026f8b3b317eb313c6d09000fb
Tester <seumworldinbangladesh@gmail.com>
2021-05-02 04:54:07 +0600

    commented two important address

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,6 +6,11 @@ function SearchPage() {
     // eslint-disable-next-line
     const [{ term }, dispatch] = useStateValue();
     return (
+
+        // https://developers.google.com/custom-search/v1/overview
+
+        // https://cse.google.com/cse/create/new
+
         <div className='searchPage'>
             <div className='searchPage__header'>
                 <h1>{term}</h1>
