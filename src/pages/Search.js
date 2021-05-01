6d03ad88323f040aa46b310b373e6b3042679ca7
Tester <seumworldinbangladesh@gmail.com>
2021-05-01 13:27:39 +0600

    add mic icon fixed input not showing problem

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './Search.css';
 //import { SearchIcon } from '@material-ui/icons/Search';
  import SearchIcon from '@material-ui/icons/Search';
+ import MicIcon from '@material-ui/icons/Mic';
 
 function Search() {
     return (
         <div className='search'>
             <div className='search__input'>
-                <SearchIcon className='search__inputIcon'>
+                <SearchIcon className='search__inputIcon'/>
 
                     <input/>
 
-                </SearchIcon>
+                    <MicIcon/>
+
+                
 
             </div>
             
