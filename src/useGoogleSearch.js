ef5608f2618a6b8efb5130ed442d41d0ebf002d4
Tester <seumworldinbangladesh@gmail.com>
2021-05-02 09:32:07 +0600

    little uri pattern miss make very problem when works with api be careful

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import API_KEY from './keys';
 
-const CONTEXT_KEY = '9c02f919021fa6113';
+const CONTEXT_KEY = 'a7f7ec27e86078ebf';
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
@@ -14,7 +14,8 @@ const useGoogleSearch = (term) => {
             fetch(
                 // GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
                 // `https://www.googleapis.com/customersearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}:omuauf_lfve&q=${term}`
-                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}:omuauf_lfve&q=cars&callback=${term}`
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+                // `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}:omuauf_lfve&q=cars&callback=${term}`
             )
             .then(
                 response => response.json()
