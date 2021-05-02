78e304ffa2744de8eecb6f9e9a240f3e4460ac5f
Tester <seumworldinbangladesh@gmail.com>
2021-05-02 09:22:47 +0600

    some improovement

--- src/keys.js
+++ src/keys.js
@@ -1,3 +1,4 @@
  export const API_KEY = "AIzaSyDmfs1NFcNxKNG4rIEjurYsvoLm_2yOXLk";
 //export const API_KEY = "AIzaSyB7I6KatQUH3Mso-W4jfNdocBMbD7XOY7M";
+//AIzaSyDmfs1NFcNxKNG4rIEjurYsvoLm_2yOXLk
 export default API_KEY;
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import API_KEY from './keys';
 
-const CONTEXT_KEY = "9c02f919021fa6113";
+const CONTEXT_KEY = '9c02f919021fa6113';
 
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
@@ -12,7 +12,9 @@ const useGoogleSearch = (term) => {
         //effect
         const fetchData = async() => {
             fetch(
-                `https://www.googleapis.com/customersearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+                // GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+                // `https://www.googleapis.com/customersearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}:omuauf_lfve&q=${term}`
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}:omuauf_lfve&q=cars&callback=${term}`
             )
             .then(
                 response => response.json()
